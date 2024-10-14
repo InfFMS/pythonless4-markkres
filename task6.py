5868fef8058464bb79109760427a1a8bae2b4312
Крестовников Марк Алексеевич <m.krestovnikov@main.nsu.ru>
2024-10-14 10:13:01 +0700

    all done

--- task6.py
+++ task6.py
@@ -8,4 +8,27 @@
 # 25 15
 # Вывод:
 # 5 3
-
+M,N=map(int,input().split())
+def PRIME(n):
+    mas=[]
+    for i in range(2,n+1):
+        b=0
+        for j in range(2,i):
+            if i%j==0:
+                b=1
+        if b==0: mas.append(i)
+    return mas
+mas=PRIME(max(N,M))
+def raslosh(n):
+    if n==1:
+        return ''
+    else:
+        for i in mas:
+            if n%i==0:
+                return raslosh(int(n/i))+str(i)
+                break
+k=raslosh(max(N,M))
+for i in range(len(k)):
+    if N%int(k[i])==0 and M%int(k[i])==0:
+        N,M=int(N/int(k[i])),int(M/int(k[i]))
+print(N,M)
--- task8.py
+++ task8.py
@@ -7,3 +7,24 @@
 # Вывод:
 # 2*3*3*3*7
 
+def PRIME(n):
+    mas=[]
+    for i in range(2,n+1):
+        b=0
+        for j in range(2,i):
+            if i%j==0:
+                b=1
+        if b==0: mas.append(i)
+    return mas
+n=int(input())
+mas=PRIME(n)
+def raslosh(n):
+    if n==1:
+        return ''
+    else:
+        for i in mas:
+            if n%i==0:
+                return raslosh(int(n/i))+str(i)+'*'
+                break
+print(raslosh(n)[:-1])
+    
